11278eb3f263630f4cb0ac3b33d4dfb4be0f54b3
jkerry <john@kerryhouse.net>
2015-12-30 00:00:28 -0500

    removing the time patch

--- js/marathonSchedule.js
+++ js/marathonSchedule.js
@@ -1,5 +1,5 @@
 var marathonSchedule = [
-  {huluLink:"http://www.hulu.com/embed.html?eid=jpsbq2p_-oo4z6untu9iua",episodeName:"Where is Everybody?",fireTime:new Date("2015-12-29 23:30:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=jpsbq2p_-oo4z6untu9iua",episodeName:"Where is Everybody?",fireTime:new Date("2015-12-30 19:00:00")},
   {huluLink:"http://www.hulu.com/embed.html?eid=ddpcmf70zugzurl_1qocea",episodeName:"One For the Angels",fireTime:new Date("2015-12-30 19:30:00")},
   {huluLink:"http://www.hulu.com/embed.html?eid=8nghaweagnprmauybk-zng",episodeName:"Mr. Denton on Doomsday",fireTime:new Date("2015-12-30 20:00:00")},
   {huluLink:"http://www.hulu.com/embed.html?eid=ivhwtz-nhxfjcgvqwe-7eg",episodeName:"The Sixteen-Millimeter Shrine",fireTime:new Date("2015-12-30 20:30:00")},
