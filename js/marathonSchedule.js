850e0cb8aa884f9bcce2e80b6f44fcacbbf11b95
jkerry <john@kerryhouse.net>
2015-12-29 23:47:46 -0500

    changing to ISO date format

--- js/marathonSchedule.js
+++ js/marathonSchedule.js
@@ -1,12 +1,12 @@
 var marathonSchedule = [
-{huluLink:"http://www.hulu.com/embed.html?eid=jpsbq2p_-oo4z6untu9iua",episodeName:"Where is Everybody?",fireTime:new Date("Dec 29 2015  11:30PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=ddpcmf70zugzurl_1qocea",episodeName:"One For the Angels",fireTime:new Date("Dec 30 2015  7:30PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=8nghaweagnprmauybk-zng",episodeName:"Mr. Denton on Doomsday",fireTime:new Date("Dec 30 2015  8:00PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=ivhwtz-nhxfjcgvqwe-7eg",episodeName:"The Sixteen-Millimeter Shrine",fireTime:new Date("Dec 30 2015  8:30PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=r-qx8xc8msandij-xpp8lg",episodeName:"Walking Distance",fireTime:new Date("Dec 30 2015  9:00PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=gvm2xof7yrybetu81weagw",episodeName:"Escape Clause",fireTime:new Date("Dec 30 2015  9:30PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=jmg1euoboysbf8uks4w2ia",episodeName:"The Lonely",fireTime:new Date("Dec 30 2015 10:00PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=p1yndnfol2xvxa8lbowpxw",episodeName:"Time Enough at Last",fireTime:new Date("Dec 30 2015 10:30PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=87hl3jewd-qyin-vrkgsww",episodeName:"Perchance to Dream",fireTime:new Date("Dec 30 2015 11:00PM")},
-{huluLink:"http://www.hulu.com/embed.html?eid=je2jgweszp2_k_9rzhhwvg",episodeName:"Judgment Night",fireTime:new Date("Dec 30 2015 11:30PM")}
+  {huluLink:"http://www.hulu.com/embed.html?eid=jpsbq2p_-oo4z6untu9iua",episodeName:"Where is Everybody?",fireTime:new Date("2015-12-29T23:30:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=ddpcmf70zugzurl_1qocea",episodeName:"One For the Angels",fireTime:new Date("2015-12-30T19:30:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=8nghaweagnprmauybk-zng",episodeName:"Mr. Denton on Doomsday",fireTime:new Date("2015-12-30T20:00:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=ivhwtz-nhxfjcgvqwe-7eg",episodeName:"The Sixteen-Millimeter Shrine",fireTime:new Date("2015-12-30T20:30:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=r-qx8xc8msandij-xpp8lg",episodeName:"Walking Distance",fireTime:new Date("2015-12-30T21:00:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=gvm2xof7yrybetu81weagw",episodeName:"Escape Clause",fireTime:new Date("2015-12-30T21:30:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=jmg1euoboysbf8uks4w2ia",episodeName:"The Lonely",fireTime:new Date("2015-12-30T22:00:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=p1yndnfol2xvxa8lbowpxw",episodeName:"Time Enough at Last",fireTime:new Date("2015-12-30T22:30:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=87hl3jewd-qyin-vrkgsww",episodeName:"Perchance to Dream",fireTime:new Date("2015-12-30T23:00:00")},
+  {huluLink:"http://www.hulu.com/embed.html?eid=je2jgweszp2_k_9rzhhwvg",episodeName:"Judgment Night",fireTime:new Date("2015-12-30T23:30:00")}
 ];
